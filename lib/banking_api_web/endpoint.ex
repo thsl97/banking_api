adc2a0ed67a85e696f427208ab299e0240259ab9
Thalysson Henrique <thalysson-henrique_pb@hotmail.com>
2021-04-16 08:53:32 -0300

    remove socket and static pluggs from web app

--- lib/banking_api_web/endpoint.ex
+++ lib/banking_api_web/endpoint.ex
@@ -10,20 +10,6 @@ defmodule BankingApiWeb.Endpoint do
     signing_salt: "gAr2Hl4S"
   ]
 
-  socket "/socket", BankingApiWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :banking_api,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
@@ -35,7 +21,7 @@ defmodule BankingApiWeb.Endpoint do
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
   plug Plug.Parsers,
-    parsers: [:urlencoded, :multipart, :json],
+    parsers: [:json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
