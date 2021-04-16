2ec7d33e86142215b9b460699ed573d0610a3b39
Thalysson Henrique <thalysson-henrique_pb@hotmail.com>
2021-04-16 10:04:15 -0300

    add timestamps to bank_accounts table

--- priv/repo/migrations/20210416125137_create_bank_account.exs
+++ priv/repo/migrations/20210416125137_create_bank_account.exs
@@ -5,6 +5,8 @@ defmodule BankingApi.Repo.Migrations.CreateBankAccount do
     create table(:bank_accounts, primary_key: false) do
       add :user_id, :uuid, [references(:users), primary_key: true]
       add :balance, :integer
+
+      timestamps()
     end
   end
 end
